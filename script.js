7b4f19d7c28505b01dc8205d20d3193f27ab56fc
さな <alamodemitek37@gmail.com>
2025-12-01 14:15:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 $(function () {
-
-    // MENUボタンクリックで表示/非表示
     $(".menu-button").on("click", function (e) {
-        e.stopPropagation();  // クリックが外側判定されないように
+        e.stopPropagation(); 
         $(".dropdown-menu").slideToggle(200);
     });
 
-    // ドロップダウンメニュー内クリックは閉じない
     $(".dropdown-menu").on("click", function (e) {
         e.stopPropagation();
     });
 
-    // MENUボタン・メニュー以外をクリックしたら閉じる
     $(document).on("click", function () {
         $(".dropdown-menu").slideUp(200);
     });
 
 });
+

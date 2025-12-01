3a111ec1be24c914e3c7df80a479065f5a6734bc
さな <alamodemitek37@gmail.com>
2025-12-01 13:34:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -47,7 +47,21 @@ $(document).ready(function() {
 
     loadImages('image');
     $overlay.stop(true,true).fadeOut(300);
+
+    $(".menu-button").on("click", function (e) {
+        e.stopPropagation(); 
+        $(".dropdown-menu").slideToggle(200);
+    });
+
+    $(".dropdown-menu").on("click", function (e) {
+        e.stopPropagation();
+    });
+
+    $(document).on("click", function () {
+        $(".dropdown-menu").slideUp(200);
+    });
 });
+
 
 
 
